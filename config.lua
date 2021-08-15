d8c9351d6e657018cee20d538eac262b13d6dd20
SlappeDev <88968058+SlappeDev@users.noreply.github.com>
2021-08-15 12:56:54 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,60 @@
+SlappeBlackmarket = {}
+
+SlappeBlackmarket.webhook = ''
+SlappeBlackmarket.Image = '' 
+
+SlappeBlackmarket.BlackmarketText = 'Druk op ~r~[E]~w~ om in te loggen' -- De tekst die je ziet als je dicht in de buurt van de marker komt -- 
+
+SlappeBlackmarket.Wapen = { -- Wapen Lijst -- -- Je kan hier alleen wapens veranderen -- 
+    [1] = { 
+        naam = 'AK-47',
+        wapennaam = 'WEAPON_ASSAULTRIFLE',
+        prijs = 3500000,
+        ammo = 49,
+        beschrijving = '3.500.000'
+    },
+    [2] = { 
+        naam = 'Special Carbine',
+        wapennaam = 'WEAPON_SPECIALCARBINE',
+        prijs = 7500000,
+        ammo = 49,
+        beschrijving = '7.500.000'
+    },
+    [3] = { 
+        naam = 'Shotgun',
+        wapennaam = 'WEAPON_SAWNOFFSHOTGUN',
+        prijs = 2500000,
+        ammo = 49,
+        beschrijving = '2.500.000'
+    },
+    [4] = { 
+        naam = 'Musket',
+        wapennaam = 'WEAPON_MUSKET',
+        prijs = 35000000,
+        ammo = 49,
+        beschrijving = '35.000.000'
+    },
+    [5] = { 
+        naam = 'Tommy Gun',
+        wapennaam = 'WEAPON_GUSENBERG',
+        prijs = 8500000,
+        ammo = 49,
+        beschrijving = '8.500.000'
+    },
+    [6] = { 
+        naam = 'Mini AK',
+        wapennaam = 'WEAPON_COMPACTRIFLE',
+        prijs = 5000000,
+        ammo = 49,
+        beschrijving = '5.000.000'
+    },
+}
+
+
+SlappeBlackmarket.Locatie = { -- De locatie van de Blackmarket --
+    [1] = { -- Locatie Blackmarket
+        x = 1275.81,
+        y = -1710.69,
+        z = 54.77,
+    }
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Maxim Scripts'
+description 'maximdrugs'
+version '1.0.0'
+
+client_scripts {
+    'client/client.lua',
+    'config.lua',
+
+}
+
+server_scripts {
+    'server/server.lua',
+    'config.lua',
+
+}
